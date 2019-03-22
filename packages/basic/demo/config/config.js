7678611c5bdc9e61c0546dd14ed4c5430cbfb018
waheng chang <wahengchang@gmail.com>
2019-03-22 12:07:48 +0800

    Fix fontawesome not found problem

--- packages/basic/demo/config/config.js
+++ packages/basic/demo/config/config.js
@@ -1,35 +1,10 @@
 import 'babel-polyfill';
+import Vue from "vue";
 import { configure } from '@storybook/vue';
 import { setOptions } from '@storybook/addon-options';
 
-
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-library.add(fas);
-
-import Vue from 'vue';
-import VueBus from 'vue-bus';
-Vue.use(VueBus);
-
-import Vuetify from 'vuetify';
-Vue.use(Vuetify);
-import 'vuetify/dist/vuetify.min.css';
-
-import ECharts from 'vue-echarts';
-Vue.component('chart', ECharts);
-
-import '../../src/styles/base.styl';
-
-import components from '../../src/components';
-components.forEach((component) => {
-  if (component.name) {
-    Vue.component(component.name, component);
-  }
-});
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
+import buildConfig from '../../src/build'
+buildConfig.install(Vue)
 
 setOptions({
   name: 'Lazybee',
--- packages/basic/src/main.js
+++ packages/basic/src/main.js
@@ -1,27 +1,10 @@
-import Vue from "vue";
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-
 import App from "./App.vue";
+import buildConfig from './build'
+import Vue from "vue";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-library.add(fas);
+buildConfig.install(Vue)
 
 Vue.config.productionTip = false;
-Vue.use(Vuetify)
-
-import components from './components';
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-components.forEach((component) => {
-  if (component.name) {
-    Vue.component(component.name, component);
-  }
-});
 
 new Vue({
   render: h => h(App)
