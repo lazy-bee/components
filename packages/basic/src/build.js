7f1a1b8941e8379377b15f4ba5ce83fcde9338e0
waheng chang <wahengchang@gmail.com>
2019-03-22 12:07:48 +0800

    Fix fontawesome not found problem

--- packages/basic/src/build.js
+++ packages/basic/src/build.js
@@ -1,24 +1,33 @@
 import VueBus from 'vue-bus';
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
+// vuetify
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import 'vuetify/dist/vuetify.min.css'
 
+// echart
 import ECharts from 'vue-echarts/components/ECharts';
 import 'echarts/lib/chart/bar';
 import 'echarts/lib/chart/line';
 import 'echarts/lib/chart/pie';
 import 'echarts/lib/component/tooltip';
-import './styles/base.styl';
-import components from './components';
-import 'element-ui/lib/theme-chalk/index.css';
 
+// front awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+library.add(fas);
+
+// components of this project
+import components from './components';
+import './styles/base.styl';
 const install = function(Vue) {
   Vue.use(VueBus);
   Vue.use(Vuetify);
 
   Vue.component('chart', ECharts);
 
+  Vue.component('font-awesome-icon', FontAwesomeIcon);
+  
   components.forEach((component) => {
     if (component.name) {
       Vue.component(component.name, component);
