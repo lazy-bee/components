94765577098a8509f68a4a83c9e44c9e46e0ce64
waheng chang <wahengchang@gmail.com>
2019-05-27 18:09:04 +0800

    Fix: not cleaning selectedpool when updating props.poolItem

---
+++ packages/twoSideSelect/tests/unit/TwoSideSelect.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from "@vue/test-utils";
+import TwoSideSelect from "@/components/TwoSideSelect/index.vue";
+
+describe("TwoSideSelect.vue", () => {
+  it("should clean state.selectedPoolItems when updating props poolItems", (done) => {
+    const msg = "new message";
+    const poolItem1 = [{label: 'fooLabel1', value: 'fooValue1'}]
+    const poolItem2 = [{label: 'fooLabel2', value: 'fooValue2'}]
+    const wrapper = shallowMount(TwoSideSelect, {
+      propsData: {
+        items: [],
+        poolItems: [...poolItem1]
+      }
+    });
+
+    expect(wrapper.vm.$data.selectedPoolItems.length).toBe(0)
+    wrapper.vm.onSelectPoolHandler({ value: 'fooValue'})
+    expect(wrapper.vm.$data.selectedPoolItems.length).toBe(1)
+    wrapper.setProps({ poolItems: [...poolItem2] })
+    expect(wrapper.vm.$data.selectedPoolItems.length).toBe(0)
+    done()
+  });
+});
